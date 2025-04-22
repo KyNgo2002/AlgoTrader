752d89988dc852eac5625b0647fc329404302174
Ky <kyngo992@gmail.com>
2025-04-21 20:05:48 -0700

    ENV keys

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 from alpaca.data.historical import StockHistoricalDataClient
 from alpaca.data.requests import StockBarsRequest
 from alpaca.data.timeframe import TimeFrame
-import datetime
+from datetime import datetime
+
+# Loading environment variables
+import os
+from dotenv import load_dotenv
+
+# Loading API and SECRET keys
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # Create stock data client
-client = StockHistoricalDataClient()
+client = StockHistoricalDataClient(api_key = API_KEY, secret_key = SECRET_KEY)
 
 # Creating request parameter object
 requestParams = StockBarsRequest(
-    symbol_or_symbols = ["USD"],
+    symbol_or_symbols = ["GOOG"],
     timeframe = TimeFrame.Day,
-    start = datetime(2025, 4, 20),
-    end = datetime(2025, 4, 21)
+    start = datetime(2022, 9, 1),
+    end = datetime(2022, 10, 1)
 )
 
 # Retrieve stock bars in a data frame
 stockBars = client.get_stock_bars(requestParams)
 
 # Convert to dataframe 
-stockBars.df
+print(stockBars.df)
